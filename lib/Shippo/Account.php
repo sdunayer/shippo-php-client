263e3d71cf8422117e4423cb70f892d6e54be4b8
Sid Dunayer <sdunayer@interserv.com>
2024-09-06 13:34:52 -0400

    Add account support

--- lib/Shippo/Account.php
+++ lib/Shippo/Account.php
@@ -5,6 +5,14 @@ class Shippo_Account extends Shippo_ApiResource
 	/**
 	 *
 	 */
+    public static function classUrl($class)
+    {
+        return "/shippo-accounts";
+    }
+    
+	/**
+	 *
+	 */
 	public static function create(?array $params = null, ?string $apiKey = null): Shippo_Account
 	{
 		$class = get_class();
