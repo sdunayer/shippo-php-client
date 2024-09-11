d3e267090682ac6f3a56550b95367f64da3ff854
Sid Dunayer <sdunayer@interserv.com>
2024-09-11 13:26:59 -0400

    Add account support

--- lib/Shippo/Account.php
+++ lib/Shippo/Account.php
@@ -18,4 +18,31 @@ class Shippo_Account extends Shippo_ApiResource
 		$class = get_class();
 		return self::_scopedCreate($class, $params, $apiKey);
 	}
+    
+	/**
+	 *
+	 */
+	public static function listAll(?array $params = null, ?string $apiKey = null): Shippo_Account
+	{
+		$class = get_class();
+		return self::_scopedAll($class, $params, $apiKey);
+	}
+    
+	/**
+	 *
+	 */
+	public static function retrieve(string $id, ?string $apiKey = null): Shippo_Account
+	{
+		$class = get_class();
+		return self::_scopedRetrieve($class, $id, $apiKey);
+	}
+    
+	/**
+	 *
+	 */
+	public static function update(string $id, array $params, ?string $apiKey = null): Shippo_Account
+	{
+		$class = get_class();
+		return self::_scopedUpdate($class, $id, $params, $apiKey);
+	}
 }
